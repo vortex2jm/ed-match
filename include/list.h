cf31c4a03508d6779b42cb2df29311834c4c1e38
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-15 20:47:50 -0300

    :sparkles: Creating generic list entity

---
+++ include/list.h
@@ -0,0 +1,18 @@
+#ifndef list_h
+#define list_h
+
+typedef struct list List;
+typedef void (*print_list)(void * element);
+typedef int (*list_compare)(void * element, void * key);
+
+List * CreateVoidList();
+
+List * PushList(List * list, void * element);
+
+void PrintList(List * list, print_list print_callback);
+
+void ListRemove(List * list, list_compare remove_callback, void * key);
+
+void DestructList(List * list);
+
+#endif
---
+++ src/list.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/list.h"
+
+typedef struct cell Cell;
+
+struct list{
+    Cell * first;
+    Cell * last;
+};
+
+struct cell{
+    Cell * next;
+    void * element; 
+};
+
+//=================================================================//
+List * CreateVoidList(){
+
+    List * list = malloc(sizeof(List));
+    list->first = NULL;
+    list->last = NULL;
+
+    return list;
+}
+
+//=================================================================//
+List * PushList(List * list, void * element){
+
+    Cell * newCell = malloc(sizeof(Cell));
+    newCell->element = element;
+    newCell->next = NULL;
+
+    if(!list->first){
+        list->first = newCell;
+        list->last = newCell;
+    }
+    else{
+        list->last->next = newCell;
+        list->last = newCell;
+    }
+    return list;
+}
+
+//=================================================================//
+void PrintList(List * list, print_list print_callback){
+
+    if(!list) return;
+
+    Cell * current = list->first;
+
+    while(current){
+        print_callback(current->element);
+        current = current->next;
+    }
+}
+
+//=================================================================//
+void ListRemove(List * list, list_compare remove_callback, void * key){
+
+    if(!list) return;
+
+    Cell * current = list->first;
+    Cell * next = NULL;
+    Cell * previous = NULL;
+
+    while(current){
+        
+        next = current->next;
+
+        if(remove_callback(current->element, key)){
+            
+            if(current == list->first){
+                list->first = list->first->next;
+            }
+            else if(current == list->last){
+                list->last = previous;
+                previous->next = NULL;
+            }
+            else{
+                previous->next = current->next;
+            }
+
+            free(current);
+        }
+        previous = current;
+        current = next;
+    }
+}
+
+//=================================================================//
+void DestructList(List * list){
+
+    Cell * current = list->first;
+    Cell * aux;
+
+    if(list){
+        while(current){
+            aux = current->next;
+            free(current);
+            current = aux;
+        }
+        free(list);
+    }
+}
